9963c727b388ba0475d2d848b7e57f19793d2b89
George Nidekker <georgenidekker@gmail.com>
2024-07-22 15:02:06 +0300

    Добавлена функция управления юзерами в Админке

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,29 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
-from flask_login import LoginManager, current_user, login_required
-from flask_migrate import Migrate
-
 from app.admin.views import blueprint as admin_blueprint
 from app.user.views import blueprint as user_blueprint
 from app.plans.views import blueprint as plans_blueprint
 from app.main.views import blueprint as main_blueprint
+
+from flask import Flask
+from flask_login import LoginManager
+from flask_migrate import Migrate
+
 from app.user.models import User
 from app.db import db
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
-    db.init_app(app)
 
+    db.init_app(app)
     migrate = Migrate(app, db)
 
     login_manager = LoginManager()
     login_manager.init_app(app)
     login_manager.login_view = 'user.login'
-    
+
     app.register_blueprint(user_blueprint)
-    app.register_blueprint(admin_blueprint)
     app.register_blueprint(plans_blueprint)
+    app.register_blueprint(admin_blueprint)
     app.register_blueprint(main_blueprint)
 
     @login_manager.user_loader
--- app/admin/views.py
+++ app/admin/views.py
@@ -1,34 +1,44 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
-from flask_login import current_user
-
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import login_required, current_user
 from app.user.decorators import admin_required
-from app.plans.models import MediaPlan
-from collections import defaultdict
-from decimal import Decimal
+from app.user.models import User
+from app.db import db
+from app.admin.forms import UserForm
 
 blueprint = Blueprint('admin', __name__, url_prefix='/admin')
 
-@blueprint.route('/')
+@blueprint.route('/users', methods=['GET', 'POST'])
 @admin_required
-def admin_index():
-    if not current_user.is_admin:
-        flash('Доступ запрещен', 'danger')
-        return redirect(url_for('plan.media_plans'))
-    
-    #Статистика по источникам
-    sources_count = defaultdict(int)
-    budgets = defaultdict(float)
-    plans = MediaPlan.query.all()
+def manage_users():
+    form = UserForm()
+    users = User.query.all()
+    user_id = request.args.get('user_id')
 
-    for plan in plans:
-        sources_count[plan.source] += 1
-        budgets[plan.source] += float(plan.budget)
+    if request.method == 'POST' and form.validate_on_submit():
+        if user_id:
+            user = User.query.get_or_404(user_id)
+            form.populate_obj(user)
+            flash('Пользователь успешно обновлен!', 'success')
+        else:
+            new_user = User(username=form.username.data, email=form.email.data, role=form.role.data)
+            db.session.add(new_user)
+            flash('Пользователь успешно добавлен!', 'success')
+        db.session.commit()
+        return redirect(url_for('admin.manage_users'))
 
-    source_stats = sorted(sources_count.items())
-    budget_stats = sorted(budgets.items())
+    if user_id:
+        user = User.query.get_or_404(user_id)
+        form = UserForm(obj=user)
+        form.user_id.data = user.id
 
-    title = "Панель управления"
-    return render_template('admin/index.html', 
-                           page_title=title,
-                           source_stats=source_stats,
-                           budget_stats=budget_stats)
+    return render_template('admin/manage_users.html', users=users, form=form)
+
+@blueprint.route('/users/delete/<int:user_id>', methods=['POST'])
+@admin_required
+def delete_user(user_id):
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    flash('Пользователь успешно удален!', 'success')
+    return redirect(url_for('admin.manage_users'))
+
